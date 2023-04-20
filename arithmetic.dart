f99be64b126965381fb8328f655673aceef2d09d
prashansa Joshi <joshiprashansa11@gmail.com>
2023-04-20 16:48:40 -0700

    Arithmetic Operation

---
+++ arithmetic.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  var add = a + b;
+  print("The sum is $add");
+
+  var sub = a - b;
+  print("The subtraction is $sub");
+
+  var mul = a * b;
+  print("The multiplication is $mul");
+
+  var div = a / b;
+  print("The division is $div");
+}
--- variables.dart
+++ variables.dart
@@ -1,10 +1,13 @@
 void main() {
+
+  //Declaring Variables
   String name = "John";
   String address = "USA";
   num age = 20;
   num height = 5.9;
   bool isMarried = false;
 
+  //Printing Variables value
   print("Name is $name");
   print("Address is $address");
   print("Age is $age");
